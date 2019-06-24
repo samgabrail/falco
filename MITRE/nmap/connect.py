3fe986790c8ccf9be34c63f304eae4dc516c466c
SamG <sam.gabrail@sysdig.com>
2019-06-24 13:44:14 -0400

    Fixed aws instance stopping

--- MITRE/nmap/connect.py
+++ MITRE/nmap/connect.py
@@ -5,7 +5,7 @@ k = paramiko.RSAKey.from_private_key_file("/app/key/throwAway.pem")
 c = paramiko.SSHClient()
 c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 print "connecting"
-c.connect( hostname = "ec2-18-233-150-46.compute-1.amazonaws.com", username = "ubuntu", pkey = k )
+c.connect( hostname = "ec2-18-206-229-215.compute-1.amazonaws.com", username = "ubuntu", pkey = k )
 print "connected"
 with SCPClient(c.get_transport()) as scp:
         scp.put('my_file.txt', '/home/ubuntu/my_file.txt') # Copy my_file.txt to the server
